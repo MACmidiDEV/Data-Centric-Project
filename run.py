df27c93803fae215df79a24b1cd677312ee3e9eb
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-03-27 23:22:19 -0400

    Flask setUP-Routed

--- run.py
+++ run.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("app.html")
 
-# if __name__ == '__main__':
-#     app.run(host=os.environ.get('IP'),
-#             port=int(os.environ.get('PORT')),
-#             debug=True)
+@app.route("/about")
+def landing():
+    return render_template("landingPage.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
       
